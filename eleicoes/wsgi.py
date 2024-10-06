0a35f63e5e5b9498327f8f2897ca02dbc650e154
Éder Pires <ederpbj@gmail.com>
2024-10-06 16:49:10 -0300

    Procfile

--- eleicoes/wsgi.py
+++ eleicoes/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eleicoes.settings")
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eleicoes.settings')
 application = get_wsgi_application()
+
