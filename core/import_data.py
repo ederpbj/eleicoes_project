d26d48a06f390fc54f5d42c356d3d9f895e2cf03
Éder Pires <ederpbj@gmail.com>
2024-10-21 18:06:35 -0300

    export csv

--- core/import_data.py
+++ core/import_data.py
@@ -1,34 +1,21 @@
-import os
-import sys
-import django
 import pandas as pd
-import datetime
 
-# Adicionar o diretório raiz do projeto ao PYTHONPATH
-sys.path.append('/Users/user/dev/Python/Django/eleicoes_project')
-
-# Configurar o Django no script
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eleicoes.settings')
-django.setup()
-
-# Agora é possível importar o modelo
-from core.models import LocalVotacao
 
 def importar_dados():
-    url = 'https://docs.google.com/spreadsheets/d/1lzAXDWS4gkCrDW6uPqfPRFWsBmzNEbaZ/edit?usp=sharing&ouid=111178866198443903759&rtpof=true&sd=true'
-    df = pd.read_excel(url, engine='openpyxl')
+    # URL de exportação do Google Sheets
+    url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTMcNDckORxHEEy_VKuFU8bHsYVNh-EPtVY35Nq7FL2SlEhNulj5PVMnDTtRijsvA/pub?output=csv'
 
-    # Carregar a planilha Excel
-    #df = pd.read_excel('core/Dados_eleições_2024.1.xlsx', engine='openpyxl')
-
+    # Carregar o CSV diretamente da URL
+    df = pd.read_csv(url)
 
     # Mostrar os nomes das colunas para verificar se estão corretos
-    print("Colunas encontradas no arquivo Excel:", df.columns)
+    print("Colunas encontradas no arquivo CSV:", df.columns)
 
     # Iterar pelas linhas do DataFrame e criar objetos LocalVotacao
     for _, row in df.iterrows():
         # Garantir que `data_instalacao` nunca seja nulo
-        data_instalacao = pd.to_datetime(row['DATA DA INSTALAÇÃO'], errors='coerce').date() if pd.notna(row['DATA DA INSTALAÇÃO']) else None
+        data_instalacao = pd.to_datetime(row['DATA DA INSTALAÇÃO'], errors='coerce').date() if pd.notna(
+            row['DATA DA INSTALAÇÃO']) else None
 
         LocalVotacao.objects.create(
             cod=int(row['Cod']) if pd.notna(row['Cod']) else 0,
@@ -45,11 +32,7 @@ def importar_dados():
             nivel_prioridade=row['NÍVEL DE PRIORIDADE'] if pd.notna(row['NÍVEL DE PRIORIDADE']) else '',
             local_votacao=row['LOCAL DE VOTAÇÃO '] if pd.notna(row['LOCAL DE VOTAÇÃO ']) else '',
             status_urnas=row[' STATUS DAS URNAS'] if pd.notna(row[' STATUS DAS URNAS']) else '',
-            status_fiscalizacao=row[' STATUS FISCALIZAÇÃO'] if pd.notna(row[' STATUS DAS URNAS']) else '',
+            status_fiscalizacao=row[' STATUS FISCALIZAÇÃO'] if pd.notna(row[' STATUS FISCALIZAÇÃO']) else '',
             falta_militar=int(row['FALTA MILITAR']) if pd.notna(row['FALTA MILITAR']) else 0,
         )
     print("Dados importados com sucesso!")
-
-# Executar a importação ao rodar o script
-if __name__ == "__main__":
-    importar_dados()
