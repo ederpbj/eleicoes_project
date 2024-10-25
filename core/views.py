c4ab35c22670ba95ba7a3b65ac3fd01ad00f4283
Éder Pires <ederpbj@gmail.com>
2024-10-25 12:51:04 -0300

    numeros em negrito

--- core/views.py
+++ core/views.py
@@ -30,12 +30,13 @@ def dashboard_view(request):
     )
     fig_status_urnas.update_traces(
         textinfo='label+percent',
-        textposition='outside',  # Posiciona o texto fora das fatias
+        texttemplate='%{label}: %{percent} <b>%{value}</b>',  # Formatação com valor em negrito
+        textposition='outside',
         marker=dict(colors=['#636EFA', '#EF553B', '#00CC96', '#AB63FA', '#FFA15A'])
     )
     fig_status_urnas.update_layout(
-        height=400,  # Aumenta o tamanho do gráfico
-        margin=dict(t=110, b=40, l=40, r=40),  # Ajusta as margens
+        height=400,
+        margin=dict(t=110, b=40, l=40, r=40),
         title={'font': {'size': 24}},
         font=dict(size=18),
         legend=dict(font=dict(size=16))
@@ -50,6 +51,7 @@ def dashboard_view(request):
     )
     fig_status_fiscalizacao.update_traces(
         textinfo='label+percent',
+        texttemplate='%{label}: %{percent} <b>%{value}</b>',
         textposition='outside',
         marker=dict(colors=['#19D3F3', '#FF6692', '#B6E880', '#FF97FF', '#FECB52'])
     )
@@ -70,7 +72,8 @@ def dashboard_view(request):
     )
     fig_status_local.update_traces(
         textinfo='label+percent',
-        textposition='outside',  # Posiciona o texto fora das fatias
+        texttemplate='%{label}: %{percent} <b>%{value}</b>',
+        textposition='outside',
         marker=dict(colors=['#FF4500', '#B0E0E6'])
     )
     fig_status_local.update_layout(
@@ -99,11 +102,12 @@ def dashboard_view(request):
         names=cias,
         values=locais_votacao,
         title='Distribuição de Locais de Votação por OPM',
-        color_discrete_sequence=['#E74C3C', '#3498DB','#9B59B6', '#2ECC71', '#F1C40F']
+        color_discrete_sequence=['#E74C3C', '#3498DB', '#9B59B6', '#2ECC71', '#F1C40F']
     )
     fig_locais_votacao_cia.update_traces(
         textinfo='label+percent',
-        textposition='outside',  # Posiciona o texto fora das fatias
+        texttemplate='%{label}: %{percent} <b>%{value}</b>',
+        textposition='outside',
         marker=dict(colors=['#FF4500', '#B0E0E6', '#90EE90'])
     )
     fig_locais_votacao_cia.update_layout(
@@ -111,7 +115,7 @@ def dashboard_view(request):
         margin=dict(t=110, b=40, l=40, r=40),
         title={'font': {'size': 24}},
         font=dict(size=18),
-        legend=dict(font=dict(size=16)),
+        legend=dict(font=dict(size=16))
     )
     graph_locais_votacao_cia = fig_locais_votacao_cia.to_html()
 
